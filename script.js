bba3604d18bce4ded9226f6304fa8f124c224f2c
krons1996 <krons1996@gmail.com>
2023-09-12 20:30:23 -0300

    Atualização no script.js e arquivo index.html

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+//Mudando a cor e adicionando a class selecionado.
 const itemLista = [...document.getElementsByTagName('ol')]
-const selecionados = [...document.getElementsByClassName('.selector')]
-
-
-
 itemLista.forEach(elemento => {
-    elemento.addEventListener('click', ()=>{
-        elemento.classList.toggle('selector')
+    elemento.addEventListener('click', () => {
+        elemento.classList.toggle('selecionado')
     })
-})
-
-function myFunction() {
-    itemLista.getElementsByClassName('selector')
-    
-}
+});
 
 
-console.log(itemLista)
-console.log(selecionados)
+//Transferindo itens.
+
+const primeiraLista = document.querySelector('.primeira');
+const segundaLista = document.querySelector('.segunda');
+function transferir(){
+    //Organizo um array com os itens que foram selecionados.
+    const primeiraListaSelecionados = [...primeiraLista.querySelectorAll('.selecionado')];
+    const segundaListaSelecionados = [...segundaLista.querySelectorAll('.selecionado')];
+    //itens selecionados na primeira lista.
+    primeiraListaSelecionados.forEach( (elemento)=>{
+        
+       elemento.classList.remove('selecionado')
+       
+       segundaLista.appendChild(elemento)
+    })
+    //itens selecionados na segunda lista.  
+    segundaListaSelecionados.forEach( (elemento)=>{
+        
+        elemento.classList.remove('selecionado')
+
+        primeiraLista.appendChild(elemento)
+    })
+};
