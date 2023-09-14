1f876aeae7d9e1eb894c40327500d8c174fa8a68
krons1996 <krons1996@gmail.com>
2023-09-14 09:50:13 -0300

    first commit

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-//Mudando a cor e adicionando a class selecionado.
-const itemLista = [...document.getElementsByTagName('ol')]
-itemLista.forEach(elemento => {
-    elemento.addEventListener('click', () => {
-        elemento.classList.toggle('selecionado')
-    })
-});
 
+const minhaLista = document.getElementsByTagName('li');
+var i;
+for (i = 0; i < minhaLista.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  minhaLista[i].appendChild(span);
+}
 
-//Transferindo itens.
+const close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-const primeiraLista = document.querySelector('.primeira');
-const segundaLista = document.querySelector('.segunda');
-function transferir(){
-    //Organizo um array com os itens que foram selecionados.
-    const primeiraListaSelecionados = [...primeiraLista.querySelectorAll('.selecionado')];
-    const segundaListaSelecionados = [...segundaLista.querySelectorAll('.selecionado')];
-    //itens selecionados na primeira lista.
-    primeiraListaSelecionados.forEach( (elemento)=>{
-        
-       elemento.classList.remove('selecionado')
-       
-       segundaLista.appendChild(elemento)
-    })
-    //itens selecionados na segunda lista.  
-    segundaListaSelecionados.forEach( (elemento)=>{
-        
-        elemento.classList.remove('selecionado')
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-        primeiraLista.appendChild(elemento)
-    })
-};
+function transferir() {
+  const li = document.createElement("LI");
+  let inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Escreva algo!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  const span = document.createElement("span");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
